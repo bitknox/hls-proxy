e400c0606fdb610d2b7913ef6cf8bbbdfccede01
Johan Flensmark <jokf@itu.dk>
2023-08-26 21:18:57 +0200

    More attempts & return error if statuscode is not in range

--- http_retry/http_retry.go
+++ http_retry/http_retry.go
@@ -1,7 +1,9 @@
 package http_retry
 
 import (
+	"errors"
 	"net/http"
+	"time"
 
 	"github.com/avast/retry-go"
 	log "github.com/sirupsen/logrus"
@@ -13,9 +15,14 @@ func ExecuteRetryableRequest(request *http.Request, attempts int) (*http.Respons
 		func() error {
 			var err error
 			resp, err = http.DefaultClient.Do(request)
+			if resp.StatusCode >= 300 || resp.StatusCode < 200 {
+				return errors.New("Non 2xx status code")
+			}
+
 			return err
 		},
-		retry.Attempts(3),
+		retry.Attempts(uint(attempts)),
+		retry.Delay(time.Second),
 		retry.OnRetry(func(n uint, err error) {
 			log.Error("Retrying request after error:", err, n)
 		}),
