0a9f4ca2417d2a1051c859de8d645405218f1d26
Johan Flensmark <jokf@itu.dk>
2023-09-18 23:52:54 +0200

    Update default http client timeout to 30 seconds

--- http_retry/client.go
+++ http_retry/client.go
@@ -9,7 +9,7 @@ import (
 // DefaultHttpClient is the default http client used by the proxy
 // We set a timeout of 15 seconds for the dialer to make sure requests don't hang and block the proxy
 var DefaultHttpClient = http.Client{
-	Timeout: 10 * time.Second,
+	Timeout: 30 * time.Second,
 	Transport: &http.Transport{
 		Dial: (&net.Dialer{
 			Timeout: 15 * time.Second,
